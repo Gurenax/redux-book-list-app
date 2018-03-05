6f48499aa8184f0e917e011fbea7400dbfd19024
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-03-05 19:36:37 +1100

    Remove key from BookListItem

--- src/components/BookListItem.js
+++ src/components/BookListItem.js
@@ -1,12 +1,10 @@
 import React from 'react'
 
 const BookListItem = ({
-  key,
   onBookListItemClick,
   bookTitle
 }) => (
   <li
-    key={bookTitle}
     onClick={onBookListItemClick}
     className="list-group-item"
   >
