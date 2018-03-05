2be7ab8b52c6eeacffb666a89afe9575ca039844
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-03-05 17:31:33 +1100

    Refactor folder structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react'
-
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 
-// import logo from './logo.svg';
-import './App.css'
-import BookList from './containers/BookList'
-import BookDetail from './containers/BookDetail'
+/*
+* Main CSS
+*/
+import '../styles/App.css'
+
+/*
+* Redux Containers
+*/
+import BookList from '../containers/BookList'
+import BookDetail from '../containers/BookDetail'
 
 /*
 * Create a Redux Store for the RootReducer (needed by Provider)
 */
-import RootReducer from './reducers/index'
+import RootReducer from '../reducers/index'
 const store = createStore(RootReducer)
 
 class App extends Component {
